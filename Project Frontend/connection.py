b75721a231d26ef722ec813811f70b28e9bf150a
U Mishra <mishraum@gmail.com>
2025-08-05 15:31:54 +0530

    connection.py lines fixed

--- Project Frontend/connection.py
+++ Project Frontend/connection.py
@@ -1,20 +1,16 @@
 import os
 import pymysql.cursors
 
-#DB_HOST = os.environ.get('DB_HOST', '10.135.124.35')
-#DB_USER = os.environ.get('DB_USER')
-#DB_PASSWORD = os.environ.get('DB_PASSWORD')
-#DB_NAME = os.environ.get('DB_NAME', 'test1')  # optional default
+DB_HOST = os.environ.get('DB_HOST', '10.135.124.35')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_NAME = os.environ.get('DB_NAME', 'test1')  # optional default
 
 CONNECTION = pymysql.connect(
-    #host=DB_HOST,
-    host='10.135.124.35',
-    #user=DB_USER,
-    user='newuser',
-    #password=DB_PASSWORD,
-    password='Pass@1234',
-    #db=DB_NAME,
-    db='test1',
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    db=DB_NAME,
     port=3306,
     charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor
